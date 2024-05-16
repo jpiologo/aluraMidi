bddbf127b98c16bb0b6fe0dbc286b4b8fade1c4e
Joao Piologo <joaotpiologo@gotmail.com>
2024-05-16 17:03:07 -0300

    pequenas atualizações

---
+++ atividades.js
@@ -0,0 +1,9 @@
+//Atividade 01:
+document.querySelector('.tecla_tim');
+
+//Atividade 02:
+let tecla = document.querySelector('.tecla_tim');
+tecla.onclick = function(){
+    alert("Fui clicado!!");
+}
+

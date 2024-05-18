ac689e756e08750f065af3e197b6ae554984dc8d
João Piologo <122281207+jpiologo@users.noreply.github.com>
2024-05-18 17:16:56 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,22 +1,86 @@
-function tocaSom(idElementAudio){
-    document.querySelector(idElementAudio).play()
+function tocaSom (idElementoAudio){
+    const elemento = document.querySelector(idElementoAudio);
+
+    if (elemento === null){
+        console.log('Element not found.')
+    }
+
+    if(elemento != null && elemento.localName === 'audio'){
+        elemento.play();
+    }
+    else {
+        console.log('Elemento não encontrado ou seletor inválido.');
+    }
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0;
-
-while (contador < listaDeTeclas.length) {
-
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
-    const idAudio = `#som_${instrumento}`
+    const idAudio = `#som_${instrumento}`;//template string
 
-    tecla.onclick = function() {
+    //console.log(idAudio)
+
+    tecla.onclick = function () {
         tocaSom(idAudio);
     }
 
-    contador = contador + 1;
+    tecla.onkeydown = function(evento){
 
-    console.log(contador);
+        if(evento.code === 'Space' || evento.code === "Enter"){
+            tecla.classList.add('ativa');
+        }
+        
+    }
+
+    tecla.onkeyup = function(){
+        tecla.classList.remove('ativa');
+    }
+
 }
+
+
+
+//Atividade 01*****************************
+// let contador = 0;
+
+// const tecla = document.querySelector('.tecla_pom');
+
+// function addCount(){
+//     contador = contador + 1;
+//     console.log(contador);
+// }
+
+// tecla.onclick = addCount;
+
+//Atividade 02*****************************
+
+// const teclas = document.querySelectorAll('.tecla');
+
+// console.log(teclas);
+
+//Atividade 03*****************************
+
+// let contador = 1;
+
+// function addCount(){
+//     console.log(contador)
+//     contador++;
+// }
+
+// teclas[3].onclick = addCount;
+
+//Atividade 04 e 05*****************************
+
+// let contador = 0;
+// const listaDeTeclas = document.querySelectorAll('.tecla')
+
+//  function addCount(){
+//     console.log(contador)
+//     contador++;
+// }
+
+// for(let i = 0; i<listaDeTeclas.length; i++){
+//     listaDeTeclas[i].onclick = addCount;
+// }
